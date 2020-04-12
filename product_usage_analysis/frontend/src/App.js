7b109d7b4ae7eaee27bc592d79445ba759d51ffd
linlit <alinkondakova@gmail.com>
2020-04-12 19:19:54 +0300

    antd layout added

--- product_usage_analysis/frontend/src/App.js
+++ product_usage_analysis/frontend/src/App.js
@@ -1,27 +1,54 @@
 import React from 'react';
-import { Row, Col, Typography } from 'antd'
+import { Layout, Menu, Typography } from 'antd'
 import * as productActions from './actions'
 
 import './App.scss';
 import { connect } from 'react-redux';
 
-const { Title } = Typography
+const { SubMenu } = Menu;
+
+const { Header, Content, Footer, Sider } = Layout;
+const { Title, Text } = Typography
 
 class AppUnwrapped extends React.Component {
-  
+  state = {
+    collapsed: false,
+    active_tab: 0
+  }
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
   componentDidMount() {
     this.props.get_products_info();
   }
 
   render() {
     const { products_info = {} } = this.props; 
-    console.log(products_info)
+    
     return (
-      <div className="App">
-        <header className="App-header">
+      <Layout style={{ minHeight: '100vh' }}>
+        
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          Some parameters
+        </Sider>
+
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 , marginLeft: 24}}>
+            <Title> Stats </Title>
+          </Header>
           
-        </header>
-      </div>
+          <Content style={{ margin: '0 16px' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Some graphs
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>JetBrains © 2020</Footer>
+        </Layout>
+
+      </Layout>
     );
   }
 }
